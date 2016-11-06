71fe82b57161f3e7ec28dfdae787212de8e44b10
tianjianbo <tianjianbo@126.com>
2016-11-07 01:53:36 +0800

    前后指针

---
+++ leetcode/167. Two Sum II - Input array is sorted/O(n).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, h = numbers.size() - 1;
+        while (numbers[l] + numbers[h] != target) {
+            if (numbers[l] + numbers[h] < target) l++;
+            else h--;
+        }
+        return vector<int>({l + 1, h + 1});
+    }
+};
+
+/**
+vector初始化方法：
+vector<int> a;
+vector<int> a(10);
+vector<int> a(10, -1);
+int a[6] = {1, 2, 3, 4, 5, 6}; vector<int> b(a, a + 4);
+vector<int> a({-1, 1});
+**/
